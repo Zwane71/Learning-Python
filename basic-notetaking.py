fc26ca69f72dd3fc6dbaa721e609eca3679bf3cf
zwane71 <makhohlisatemba@gmail.com>
2025-02-05 10:43:48 +0200

    note taking on python basiscs

---
+++ basic-notetaking.py
@@ -0,0 +1,60 @@
+#1st python code
+# print('I like banna!')
+# print('apple!')
+
+#Variable / storage
+# first_name = 'Themba'
+# food = 'ramen'
+# email = '123@gmail.com'
+
+# print(f"i am{first_name}")
+# print(f"i like{food}")
+# print(f"my email is{email}")
+
+#intergers
+# age = 25
+# quantity = 3
+
+# print(f"you are {age}")
+
+#Float
+# average = 4.4
+
+#Boolean
+# is_student = True
+
+# if is_student:
+#     print("You are student")
+# else:
+#     print("you are not a student")
+
+
+
+
+# Typecasting
+
+# name = "bro"
+# age = 19
+# gpa = 3.2
+# is_student = True
+
+# gpa = int(gpa)
+# age = str(age)
+
+# age += "1"
+
+# name = bool(name)
+
+# print(name)
+
+
+
+# User Input
+name = input("what is your name: ")
+age = int(input("how old are you: "))
+
+
+age += 1
+
+print(f"hello {name}")
+print(f"you are {age}")
